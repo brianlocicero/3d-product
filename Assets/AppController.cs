9c436243e7a52c015fd8de639de983bc9fb7444d
brianlocicero <blocicero@Brians-MacBook-Air.local>
2014-05-20 15:26:56 -0700

    Init

---
+++ Assets/AppController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppController : MonoBehaviour {
+
+	public GameObject targetItem;
+	public Camera GUICamera;
+	public int rotationRate = 1;
+	public RaycastHit hit;
+	
+	private bool wasRotating = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+		if (Input.touchCount > 0)  {   //  If there are touches...
+			
+			Touch theTouch = Input.GetTouch(0);       //    Cache Touch (0)
+			Ray ray = Camera.main.ScreenPointToRay(theTouch.position);
+
+			//if( Physics.Raycast(ray,out hit,50) ) { optional restrict to object itself
+				if(Input.touchCount == 1)
+				{
+					if (theTouch.phase == TouchPhase.Began) 
+					{
+						wasRotating = false; 
+					}     
+					
+					if (theTouch.phase == TouchPhase.Moved) 
+					{
+						targetItem.transform.Rotate(theTouch.deltaPosition.y * rotationRate, -(theTouch.deltaPosition.x * rotationRate), 0, Space.World);
+						wasRotating = true;
+					}     
+				}	
+			//}
+		}
+	}
+}
